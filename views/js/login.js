da0b329338729603f8d38bfd19500865711ae21d
Saksham Sharma <saksham0808@gmail.com>
2016-11-07 00:42:12 +0530

    Implement correct ajax based login

--- views/js/login.js
+++ views/js/login.js
@@ -1,8 +1,6 @@
 function checkPassword() {
     var user = $("#username").val();
     var passwd = $("#password").val();
-    //var user = '14588';
-    //var passwd = 'abcd';
     var loginData = {
         username: user,
         password: passwd
@@ -10,60 +8,37 @@ function checkPassword() {
     if (passwd.length < 4) {
         setErrorModal("Please provide a longer password");
     } else {
-        sessionStorage.setItem('localSecret', btoa(passwd));
-        base_url = "/api";
-        url = base_url + "/login";
-        $.post(url, loginData, function(something) {
-            console.log('yaya');
-            if (something.redirect) {
-                // data.redirect contains the string URL to redirect to
-                console.log('redirecting');
-                window.location.href = data.redirect;
-                window.location = data.redirect;
-            } else {
-                console.log('na na na na');
-            }
-        }).done(function() {
-            console.log('done');
-        }).always(function() {
-            console.log('alwaysssss');
-        }).fail(function() {
-            console.log('soemtimes we fail');
-        })
-
         $.ajax({
             type: "POST",
-            url: url,
-            data: loginData,
+            url: urls.login,    // From utils.js
+            data: JSON.stringify(loginData),
             contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            success: function (response) {
-                console.log('inside');
-                if (response.d == true) {
-                    alert("You will now be redirected.");
-                    window.location = "http://www.aspsnippets.com/";
-                }
+
+            success: function (data, status, jqXHR) {
+                console.log('Data: ' + data);
+                console.log('Status: ' + status);
+                console.log(jqXHR);
+
+                // Store password in sessionStorage (not localStorage)
+                sessionStorage.setItem('password', btoa(passwd));
+
+                // Add a redirect here
             },
-            failure: function (response) {
-                console.log('outside');
-                alert(response.d);
+            error: function (jqXHR, status, error) {
+                var errorMsg = '';
+
+                // Important because JSON.parse can fail
+                try {
+                    errorMsg = (JSON.parse(jqXHR.responseText).message);
+                } catch (e) {
+                    errorMsg = error;
+                }
+                setErrorModal(errorMsg); // From utils.js
             }
         });
-        // Check password from backend
-        // If it was true, you will also get the encrypted
-        // private key and public key
-        // Save them to the local storage too
     }
 }
 
-function loginCheck() {
-    checkPassword();
-}
-
 $(document).ready(function() {
-    $("#loginButton").click(loginCheck);
-});
-
-$(document).ready(function() {
-    console.log("hello");
+    $("#loginButton").click(checkPassword);
 });
--- views/js/utils.js
+++ views/js/utils.js
@@ -2,4 +2,10 @@
 function setErrorModal(errorMsg) {
     $("#errorDescription").text(errorMsg);
     $("#errorModal").modal("show");
-}
+};
+
+base_url = '/api';
+
+urls = {
+    'login': '/api' + '/login'
+};
