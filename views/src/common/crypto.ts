4f37fb135e5bcc446c694c54562bf57081156d45
Saksham Sharma <saksham0808@gmail.com>
2016-12-11 22:13:01 +0530

    Add manually compiled sjcl and its typings

--- views/src/common/crypto.ts
+++ views/src/common/crypto.ts
@@ -1,14 +1,69 @@
-import * as sjcl from 'sjcl';
+/// <reference path="../../sjcl/sjcl.d.ts" />
 
 export class Crypto {
-  constructor(private password: string) {
+  private pubK: sjcl.SjclElGamalPublicKey;
+  private priK: sjcl.SjclElGamalSecretKey;
+
+  static hash(data: string) {
+    let bits = sjcl.hash.sha256.hash(data);
+    return sjcl.codec.hex.fromBits(bits);
   }
 
-  encrypt(data: string) {
+  static toJson(data: string) {
+    return JSON.parse(data);
+  }
+
+  static fromJson(data) {
+    return JSON.stringify(data);
+  }
+
+  constructor(private password: string,
+              pubK?: sjcl.SjclElGamalPublicKey,
+              priK?: sjcl.SjclElGamalSecretKey) {
+    this.pubK = pubK;
+    this.priK = priK;
+  }
+
+  // Key creation and storage
+  // ------------------------
+  newKey() {
+    // 6 => Paranoia value
+    let pair = sjcl.ecc.elGamal.generateKeys(256, 6);
+    this.priK = pair.sec;
+    this.pubK = pair.pub;
+  }
+
+  serializePub() {
+    let pub = this.pubK.get();
+    return sjcl.codec.base64.fromBits(pub.x.concat(pub.y));
+  }
+
+  deserializePub(serializedVal: string) {
+    this.pubK = new sjcl.ecc.elGamal.publicKey(
+      sjcl.ecc.curves.c256,
+      sjcl.codec.base64.toBits(serializedVal)
+    );
+  }
+
+  serializePriv() {
+    return sjcl.codec.base64.fromBits(this.priK.get());
+  }
+
+  deserializePriv(sec: string) {
+    let bitsec = sjcl.codec.base64.toBits(sec);
+    this.priK = new sjcl.ecc.elGamal.secretKey(
+      sjcl.ecc.curves.c256,
+      sjcl.ecc.curves.c256.field.fromBits(bitsec)
+    );
+  }
+
+  // Symmetric enc and dec
+  // ---------------------
+  encryptSym(data: string) {
     return sjcl.json.encrypt(this.password, data);
   }
 
-  decrypt(data: sjcl.SjclCipherEncrypted) {
+  decryptSym(data: sjcl.SjclCipherEncrypted) {
     try {
       return sjcl.json.decrypt(this.password, data) || '{}';
     } catch (e) {
@@ -16,13 +71,23 @@ export class Crypto {
     }
   }
 
-  toJson(data: string) {
-    // return sjcl.json.decode(data);
-    return JSON.parse(data);
+  // Asymmetric enc and dec
+  // ----------------------
+  encryptAsym(data: string): sjcl.SjclCipherEncrypted {
+    if (!this.priK) {
+      console.error('Using non-existing private key');
+      return;
+    }
+
+    sjcl.encrypt(this.pubK, data);
   }
 
-  fromJson(data) {
-    // return sjcl.json.encode(data);
-    return JSON.stringify(data);
+  decryptAsym(data: sjcl.SjclCipherEncrypted) {
+    if (!this.priK) {
+      console.error('Decryption requires public key');
+      return;
+    }
+
+    sjcl.decrypt(this.priK, data);
   }
 }
