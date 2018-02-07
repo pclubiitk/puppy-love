f9684484312a2025ed1cb0cfe7f4fbcff49a21aa
Yash Srivastav <admin@yashsriv.org>
2018-02-08 04:11:13 +0530

    Fix service worker issue

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { SwUpdate } from '@angular/service-worker';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
@@ -11,30 +10,8 @@ export class AppComponent implements OnInit {
 
   constructor(
     public snackBar: MatSnackBar,
-    private updates: SwUpdate,
   ) {}
 
   ngOnInit() {
-    this.updates.available.subscribe(event => {
-      const snackBarRef = this.snackBar.open(
-        'Updated content is available',
-        'Reload',
-        {
-          duration: 3000
-        }
-      );
-      snackBarRef.onAction().subscribe(() => {
-        this.updates.activateUpdate().then(() => location.reload(true));
-      });
-    });
-    this.updates.activated.subscribe(event => {
-      this.snackBar.open(
-        'Content is now available offline!',
-        '',
-        {
-          duration: 1000
-        }
-      );
-    });
   }
 }
