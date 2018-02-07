ec9c257f0519d910cd4bbb5b071d362fb18ae0c1
milindl <imilindluthra@gmail.com>
2018-02-07 21:34:08 +0530

    Merge submission controllers into one

--- controllers/heart.go
+++ controllers/heart.go
@@ -6,102 +6,10 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/milindl/puppy-love/models"
-
 	"github.com/gin-gonic/gin"
 
 	"gopkg.in/mgo.v2/bson"
 )
-
-func difference(oldVotes []models.Heart,
-	newVotes []models.GotHeart) []models.GotHeart {
-
-	diff := []models.GotHeart{}
-	m := map[string]int{}
-	for _, s1val := range oldVotes {
-		m[s1val.Data] = 1
-	}
-
-	for _, s2val := range newVotes {
-		if m[s2val.Data] != 1 {
-			diff = append(diff, s2val)
-		}
-	}
-
-	return diff
-}
-
-// Serve when a Heart is to be saved
-func GotHeart(c *gin.Context) {
-	id, err := SessionId(c)
-	if err != nil || id != c.Param("you") {
-		c.AbortWithStatus(http.StatusForbidden)
-		return
-	}
-
-	// Verify valid requested changes
-	info := new([]models.GotHeart)
-	if err := c.BindJSON(info); err != nil {
-		c.String(http.StatusBadRequest, "Invalid JSON")
-		log.Println(err)
-		return
-	}
-
-	// Check that user isn't voting more than 4
-	// ========================================
-	user := models.User{}
-	if err := Db.GetById("user", id).One(&user); err != nil {
-		c.AbortWithStatus(http.StatusNotFound)
-		log.Print(err)
-		return
-	}
-
-	userVotes := new([]models.Heart)
-	if err := Db.GetCollection("heart").
-		Find(bson.M{"roll": id}).
-		All(userVotes); err != nil {
-		c.AbortWithStatus(http.StatusNotFound)
-		log.Print(err)
-		return
-	}
-
-	diffHearts := difference(*userVotes, *info)
-
-	log.Print("Earlier count: ", len(*userVotes))
-	log.Print("Sent new: ", len(diffHearts))
-
-	if len(diffHearts)+len(*userVotes) > 4 {
-		c.String(http.StatusBadRequest, "More than allowed votes")
-		return
-	}
-
-	ctime := uint64(time.Now().UnixNano() / 1000000)
-
-	newHearts := []models.Heart{}
-	for _, heart := range diffHearts {
-		newHearts = append(newHearts,
-			models.Heart{
-				Id:     id,
-				Gender: heart.GenderOfSender,
-				Time:   ctime,
-				Value:  heart.Value,
-				Data:   heart.Data,
-			})
-	}
-
-	bulk := Db.GetCollection("heart").Bulk()
-	for _, heart := range newHearts {
-		bulk.Insert(heart)
-	}
-	r, err := bulk.Run()
-
-	if err != nil {
-		c.AbortWithStatus(http.StatusInternalServerError)
-		log.Println(err)
-		return
-	}
-	c.JSON(http.StatusOK, r)
-}
 
 func HeartGet(c *gin.Context) {
 	id, err := SessionId(c)
@@ -120,6 +28,7 @@ func HeartGet(c *gin.Context) {
 	// Current time
 	ctime := uint64(time.Now().UnixNano() / 1000000)
 
+	// TODO: fix bindings to be consistent across dbs
 	type AnonymVote struct {
 		Value          string `json:"v" bson:"v"`
 		GenderOfSender string `json:"genderOfSender" bson:"gender"`
